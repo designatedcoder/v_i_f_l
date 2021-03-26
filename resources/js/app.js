7523f1fd3dee7e94e99c5abf87f66c44187f48ad
harri <harri@yahoo.com>
2021-03-26 09:44:47 -0400

    Installed Laravel UI

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,25 @@
 require('./bootstrap');
+
+window.Vue = require('vue').default;
+
+import { App, plugin } from '@inertiajs/inertia-vue'
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+
+Vue.mixin({ methods: { route }});
+Vue.use(plugin)
+Vue.use(Vuetify)
+
+const el = document.getElementById('app')
+
+new Vue({
+    vuetify: new Vuetify({
+        theme: { dark: true }
+    }),
+  render: h => h(App, {
+    props: {
+      initialPage: JSON.parse(el.dataset.page),
+      resolveComponent: name => require(`./Pages/${name}`).default,
+    },
+  }),
+}).$mount(el)
